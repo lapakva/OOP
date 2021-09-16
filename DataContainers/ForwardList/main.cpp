42ced97811ec0359157add3804605cf24c859b73
Kirill Anikin <kirill_anikin@list.ru>
2021-09-16 15:50:54 +0700

    destructor and copyAssignement

--- DataContainers/ForwardList/main.cpp
+++ DataContainers/ForwardList/main.cpp
@@ -12,42 +12,74 @@ using namespace std;
 
 class Element
 {
+	static int count;
 	int Data;
 	Element *pNext;
 
 public:
 	Element(int Data, Element *pNext = nullptr) : Data(Data), pNext(pNext)
 	{
+		count++;
 		cout << "EConstructor:\t" << this << endl;
 	}
 	~Element()
 	{
+		count--;
 		cout << "EDestructor:\t" << this << endl;
 	}
 	friend class ForwardList;
 };
+int Element::count = 0;
 
 class ForwardList
 {
 	Element *Head;
+	size_t size;
 
 public:
 	ForwardList()
 	{
 		Head = nullptr;
+		size =0;
 		cout << "LConstructor:\t" << this << endl;
 	}
+
+	ForwardList(const initializer_list<int>& il):ForwardList()
+	{
+			cout<<"Type: "<< typeid(il.begin()).name()<< endl;
+			for (int const* it = il.begin(); it != il.end(); it++)
+			{
+				this->push_back(*it);
+			}
+	}
+	ForwardList(const ForwardList& other):ForwardList()
+	{
+		for(Element* Temp  = other.Head; Temp; Temp =Temp->pNext)
+			push_back(Temp->Data);
+
+	}
+
 	~ForwardList()
 	{
-		Element *Temp = Head;
+		 while (Head) pop_front();
+		// Element *Temp = Head;
 
-		while (Temp != nullptr)
-		{
-			Element *p = Temp->pNext;
-			Temp = Temp->pNext;
-			delete p;
-		}
+		// while (!Temp)
+		// {
+		// 	Element *p = Temp;
+		// 	Temp = Temp->pNext;
+		// 	delete p;
+		// }
 		cout << "LDestructor:\t" << this << endl;
+	}
+
+	//Operators:
+	ForwardList& operator=(const ForwardList& other)
+	{
+		while (Head) pop_front();
+		for(Element* Temp  = other.Head; Temp; Temp =Temp->pNext)
+			push_back(Temp->Data);
+		return *this;
 	}
 
 	//						Adding elements:
@@ -57,46 +89,58 @@ public:
 		New->pNext = Head;
 
 		Head = New;
+		size++;
 	}
 
 	void push_back(int Data)
 	{
-		Element *New = new Element(Data);
+	
 		//find last pointer to the nullptr
+		if (!Head) return push_front(Data);
 		Element *Temp = Head;
-		while (Temp->pNext != nullptr)
+		while (Temp->pNext)
 		{
 			Temp = Temp->pNext;
 		}
-		Temp->pNext = New;
+		Temp->pNext = new Element(Data);
+		size++;
 	}
-
+       //Removing elements
 	void pop_front()
 	{
 		cout << " Element to front pop: " << Head->Data << endl;
+		//remember address of element to delet
+		
+		Element* to_del = Head;
 		Head = Head->pNext;
+		delete to_del;
+		size--;
 	}
 
 	void pop_back()
 	{
 		Element *Temp = Head;
-		while ((Temp->pNext)->pNext != nullptr)
+		while (Temp->pNext->pNext != nullptr)
 		{
 			Temp = Temp->pNext;
 		}
 		cout << " Element to back pop: " << Temp->pNext->Data << endl;
+		
+		delete Temp->pNext;
 		Temp->pNext = nullptr;
+		size--;
 	}
 
 	void insert(int Data, int index)
 	{
 		Element *New = new Element(Data);
 		Element *Temp = Head;
-		Element *p = nullptr;
-
+		
 		int i = 0;
+		if (index==0)return push_front(Data);
+		if (index>=size) return push_back(Data);
 
-		while (Temp->pNext != nullptr)
+		while (Temp->pNext)
 		{
 			if (!index)
 			{
@@ -105,18 +149,15 @@ public:
 			}
 			else if (i == index - 1)
 			{
-				Element *p = Temp->pNext;
+				New->pNext = Temp->pNext;
 				Temp->pNext = New;
-				New->pNext = p;
 				break;
 			}
 			i++;
 			Temp = Temp->pNext;
 		}
-		if (Temp->pNext == nullptr)
-		{
-			cout << " No element with index " << index << " in the List" << endl;
-		}
+		size++;
+
 	}
 
 	void erase(int index)
@@ -147,23 +188,29 @@ public:
 		{
 			cout << " No element with index " << index << " in the List" << endl;
 		}
+		size--;
 	}
 
 	//					Methods:
 	void print() const
 	{
 		Element *Temp = Head;
-		while (Temp != nullptr)
-		{
-			cout << Temp << "\t" << Temp->Data << "\t" << Temp->pNext << endl;
-			Temp = Temp->pNext;
-		}
+		// while (Temp != nullptr)
+		// {
+		// 	cout << Temp << "\t" << Temp->Data << "\t" << Temp->pNext << endl;
+		// 	Temp = Temp->pNext;
+		// }
+		for(Element* Temp=Head; Temp!=nullptr; Temp=Temp->pNext)
+		{ cout << Temp << "\t" << Temp->Data << "\t" << Temp->pNext << endl;}
+		cout<<" Number of element in the list: " << size<<endl;
+		cout<<" Number of all element: " << Element::count<<endl;
 	}
 };
-
+// #define base_check
 int main()
 {
 	setlocale(LC_ALL, "");
+#ifdef base_check
 	int n;
 	cout << "Choose number of elements: ";
 	cin >> n;
@@ -171,21 +218,19 @@ int main()
 	//push_front
 	for (int i = 0; i < n; i++)
 	{
-		list.push_front(rand() % 100);
+		//list.push_front(rand() % 100);
+		list.push_back(rand() % 100);
 	}
 	list.print();
-	list.push_back(123);
-	list.print();
-	list.pop_front();
-	list.print();
-	list.pop_back();
-	list.print();
-	list.insert(666, 2);
-	list.print();
-	cout << "##############" << endl;
+	// 
+#endif //base_check
 
-	list.erase(3);
-	list.print();
-
+ForwardList list = {3,5,8,13,21};
+list.print();
+ForwardList list2 = list;
+list2.print();
+ForwardList list3;
+list3 = list2;
+list3.print();
 	return 0;
 }
